cbbf16acc2d5d2906eae3fcfe344811888768204
Ericksosa <erick_sosa@bhd.com.do>
2025-03-31 00:31:42 -0400

    Requisito funcional RF5.4

--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ app.MapControllerRoute(
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
